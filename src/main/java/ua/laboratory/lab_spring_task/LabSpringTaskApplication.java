3b98511f9ce578f4f8a1fb14717bb8b571d23920
Andrii Tyzun <andrii.tyzun@gmail.com>
2024-08-31 14:07:03 +0300

    Initial commit

---
+++ src/main/java/ua/laboratory/lab_spring_task/LabSpringTaskApplication.java
@@ -0,0 +1,13 @@
+package ua.laboratory.lab_spring_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSpringTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/laboratory/lab_spring_task/LabSpringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.laboratory.lab_spring_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
